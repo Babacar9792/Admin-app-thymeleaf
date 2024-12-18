fce4e0a0c3fb3ede31f13f00953e1480726e8d3f
Babacar9792 <babacar779792@gmail.com>
2024-12-18 12:04:58 +0000

    initialisation du projet

---
+++ src/main/java/com/belliBabs/admin_app/entity/Produit.java
@@ -0,0 +1,23 @@
+package com.belliBabs.admin_app.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "produits")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String libelle;
+    @Column()
+    private String description;
+    private Long prix;
+}
---
+++ src/main/java/com/belliBabs/admin_app/entity/Role.java
@@ -0,0 +1,20 @@
+package com.belliBabs.admin_app.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    private Long id;
+
+}
